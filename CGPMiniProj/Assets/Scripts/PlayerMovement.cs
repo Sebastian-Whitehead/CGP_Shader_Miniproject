466853088da1dd39103ae5f0dac1c643a5ddcbdd
Sebastian Whitehead <sebastianmikael2000@gmail.com>
2022-11-18 15:15:09 +0100

    Test Enviroment
    - Player Movement
    - 3rd Person Camera Controls
    - Basic Testing world

---
+++ CGPMiniProj/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerMovement : MonoBehaviour
+{
+   [Header("Movement")] public float moveSpeed;
+
+   public float groundDrag;
+   public Transform orientation;
+
+   private float _horizontalInput;
+   private float _verticalInput;
+
+   private Vector3 _moveDirection;
+   private Rigidbody _rb;
+
+   private void Start()
+   {
+      _rb = GetComponent<Rigidbody>();
+      _rb.freezeRotation = true;
+   }
+
+   private void Update()
+   {
+      MyInput();
+      SpeedControl();
+      _rb.drag = groundDrag;
+
+   }
+
+   private void FixedUpdate()
+   {
+      MovePlayer();
+   }
+
+   private void MyInput()
+   {
+      _horizontalInput = Input.GetAxisRaw("Horizontal");
+      _verticalInput = Input.GetAxisRaw("Vertical");
+   }
+
+   private void MovePlayer()
+   {
+      _moveDirection = orientation.forward * _verticalInput + orientation.right * _horizontalInput;
+      _rb.AddForce(_moveDirection.normalized * (moveSpeed * 10f), ForceMode.Force);
+   }
+
+   private void SpeedControl()
+   {
+      Vector3 flatVel = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
+
+      if (flatVel.magnitude > moveSpeed)
+      {
+         Vector3 limitedVel = flatVel.normalized * moveSpeed;
+         _rb.velocity = new Vector3(limitedVel.x, _rb.velocity.y, limitedVel.z);
+      }
+   }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CGP_Shader_Miniproject
+
+## Sources:
+## Movement & Camera Systems:
+
+### Models:
+- https://sketchfab.com/3d-models/dippers-cap-6b247c088f034d09908d4ee78c39dd18
+-https://sketchfab.com/3d-models/low-poly-hand-painted-stone-pillar-115690022c6849c2ac8f250c0da40767
